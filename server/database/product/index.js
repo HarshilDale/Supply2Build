7c45a08831a15986983d85af818165d23a388274
HarshilDale <harshil125.dale@gmail.com>
2022-02-13 18:00:18 +0530

    Add timestamps

--- server/database/product/index.js
+++ server/database/product/index.js
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  descript: { type: String, required: true },
-  catagory: { type: String, required: true },
-  photos: {
-    type: mongoose.Types.ObjectId,
-    ref: "Images",
-  },
-  price: { type: Number, default: 250, required: true },
-  addOns: [
-    {
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    descript: { type: String, required: true },
+    catagory: { type: String, required: true },
+    photos: {
       type: mongoose.Types.ObjectId,
-      ref: "Products",
+      ref: "Images",
     },
-  ],
-  shop: {
-    type: mongoose.Types.ObjectId,
-    ref: "Shops",
-    required: true,
+    price: { type: Number, default: 250, required: true },
+    addOns: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Products",
+      },
+    ],
+    shop: {
+      type: mongoose.Types.ObjectId,
+      ref: "Shops",
+      required: true,
+    },
+    reviews: {},
   },
-  reviews: {},
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const ProductModel = mongoose.model("Products", ProductSchema);
--- server/database/reviews/index.js
+++ server/database/reviews/index.js
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
 
-const ReviewSchema = mongoose.Schema({
-  product: { type: mongoose.Types.ObjectId, ref: "Products" },
-  shop: { type: mongoose.Types.ObjectId, ref: "Shops" },
-  user: { type: mongoose.Types.ObjectId, ref: "Users" },
-  rating: { type: Number, required: true },
-  reviewText: { type: String, required: true },
-  photos: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Images",
-    },
-  ],
-});
+const ReviewSchema = mongoose.Schema(
+  {
+    product: { type: mongoose.Types.ObjectId, ref: "Products" },
+    shop: { type: mongoose.Types.ObjectId, ref: "Shops" },
+    user: { type: mongoose.Types.ObjectId, ref: "Users" },
+    rating: { type: Number, required: true },
+    reviewText: { type: String, required: true },
+    photos: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Images",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const ReviewModel = mongoose.model("Reviews", ReviewSchema);
